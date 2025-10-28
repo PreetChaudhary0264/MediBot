ca831e327a30e3a68a273e24b4af07ac9200278a
Preet Kaler <preetkalerop@gmail.com>
2025-10-28 17:46:59 +0530

    c

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
   const clientPath = path.resolve(__dirname, "../client/dist");
   app.use(express.static(clientPath));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(clientPath, "index.html"));
   });
 } else {
